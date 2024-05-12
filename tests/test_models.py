5baaf7e603c0e8e7bc88cecbc4768ba15537cce7
atfuture7 <to.future7@gmail.com>
2024-05-12 18:03:00 -0400

    env needs restart

--- tests/test_models.py
+++ tests/test_models.py
@@ -104,3 +104,36 @@ class TestProductModel(unittest.TestCase):
     #
     # ADD YOUR TEST CASES HERE
     #
+
+    def test_read_a_product(self):
+        """ It should read a product """
+        product = ProductFactory()
+        product.id = None
+        product.create()
+        self.assertIsNotNone(product.id)
+        # fetch it back
+        found_product = Product.find(product.id)
+        self.assertEqual(found_product.id, product.id)
+        self.assertEqual(found_product.name, product.name)
+        self.assertEqual(found_product.description, product.description)
+        self.assertEqual(found_product.price, product.price)
+        
+    def test_update_a_product(self):
+        """ it should update a product """
+        product = ProductFactory()
+        product.id = None
+        product.create()
+        seld.assertIsNotNone(product.id)
+        # change it and save it
+        product.description = "testing"
+        original_id = product.id
+        product.update()
+        self.assertEqual(product.id, original_id)
+        self.assertEqual(product.description, "testing")
+        # fetch it back and make sure the id hasn't changed
+        # but the data did change
+        products = Product.all()
+        self.assertEqual(len(products), 1)
+        self.assertEqual(products[0].id, original_id)
+        self.assertEqual(products[0].description, "testing")
+
